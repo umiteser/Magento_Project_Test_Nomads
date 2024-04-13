384997f8615beea11e975ed262a3860b0ca685b1
umiteser <eser_umit@hotmail.com>
2024-04-13 21:10:49 +0300

    Test Message.

--- src/main/java/Step_Definitions/Hooks.java
+++ src/main/java/Step_Definitions/Hooks.java
@@ -6,14 +6,14 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
 public class Hooks {
- // @After
- // public void after(Scenario senaryo) {
- //     if (senaryo.isFailed()) {
- //         TakesScreenshot ts = (TakesScreenshot) GWD.getDriver();
- //         byte[] hafizadakiHali = ts.getScreenshotAs(OutputType.BYTES);
- //         senaryo.attach(hafizadakiHali, "image/png", "screenshot name");
- //     }
+  @After
+  public void after(Scenario senaryo) {
+      if (senaryo.()) {
+          TakesScreenshot ts = (TakesScreenshot) GWD.getDriver();
+          byte[] hafizadakiHali = ts.getScreenshotAs(OutputType.BYTES);
+          senaryo.attach(hafizadakiHali, "image/png", "screenshot name");
+      }
 
- //     GWD.quitDriver();
+      GWD.quitDriver();
     }
 }
