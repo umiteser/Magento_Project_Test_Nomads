39f05761aa7632f8798e176eda40b80bf2227f1c
Samed <samfromda01@hotmail.com>
2024-04-14 11:43:20 +0300

    Create_New_User_Steps CreatingNewUserAccount.feature and Creating_New_User_Pom are completed.

--- src/test/java/Step_Definitions/CreatingNewUserAccount/Creating_New_User_Pom.java
+++ src/test/java/Step_Definitions/CreatingNewUserAccount/Creating_New_User_Pom.java
@@ -1,11 +1,56 @@
 package Step_Definitions.CreatingNewUserAccount;
 
+import Pages.ParentPage;
 import Utilities.GWD;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class Creating_New_User_Pom {
 
     public Creating_New_User_Pom() {
         PageFactory.initElements(GWD.getDriver(), this);
+    }
+
+    @FindBy(css = "div[class='panel header'] li:nth-child(3) a")
+    WebElement register;
+    @FindBy(css = "input[id='firstname']")
+    WebElement firstName;
+    @FindBy(css = "input[id='lastname'")
+    WebElement lastName;
+    @FindBy(css = "input[id='email_address']")
+    WebElement eMail;
+    @FindBy(css = "input[id='password']")
+    WebElement pass;
+    @FindBy(css = "input[id='password-confirmation']")
+    WebElement passConf;
+    @FindBy(css = "button[type='submit'][title='Create an Account']")
+    WebElement submit;
+    @FindBy(css = "[class='messages'] div")
+    WebElement confirmation;
+
+    public WebElement getWebElement(String element) {
+        switch (element) {
+
+            case "confirmation":
+                return this.confirmation;
+            case "submit":
+                return this.submit;
+            case "pass":
+                return this.pass;
+            case "eMail":
+                return this.eMail;
+            case "passConf":
+                return this.passConf;
+            case "firstName":
+                return this.firstName;
+            case "lastName":
+                return this.lastName;
+            case "register":
+                return this.register;
+        }
+        return null;
+
+
     }
 }
--- src/test/java/Step_Definitions/CreatingNewUserAccount/Creating_New_User_Steps.java
+++ src/test/java/Step_Definitions/CreatingNewUserAccount/Creating_New_User_Steps.java
@@ -2,15 +2,20 @@ package Step_Definitions.CreatingNewUserAccount;
 
 import Pages.ParentPage;
 import Utilities.GWD;
+import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class Creating_New_User_Steps extends ParentPage {
 
-    Creating_New_User_Pom nu=new Creating_New_User_Pom();
+    Creating_New_User_Pom nu = new Creating_New_User_Pom();
+
     @Given("User navigates to the website")
     public void userNavigatesToTheWebsite() {
         GWD.getDriver().get("https://magento.softwaretestingboard.com/");
@@ -18,30 +23,48 @@ public class Creating_New_User_Steps extends ParentPage {
     }
 
     @When("User clicks on register button")
-    public void userClicksOnRegisterButton() {
-        myClick(GWD.getDriver().findElement(By.cssSelector("div[class='panel header'] li:nth-child(3) a")));
+    public void userClicksOnRegisterButton(DataTable table) {
+        List<String> x = table.asList(String.class);
+        for (String eleman : x) {
+            WebElement element = nu.getWebElement(eleman);
+            myClick(element);
+        }
+        // myClick(GWD.getDriver().findElement(By.cssSelector("div[class='panel header'] li:nth-child(3) a")));
         System.out.println("register button clicked");
     }
 
     @Then("User fills in the blanks")
-    public void userFillsInTheBlanks() {
-        mySendKeys(GWD.getDriver().findElement(By.cssSelector("input[id='firstname']")),"samo");
-        mySendKeys(GWD.getDriver().findElement(By.cssSelector("input[id='lastname']")),"sam");
-        mySendKeys(GWD.getDriver().findElement(By.cssSelector("input[id='email_address']")),"samAos@hotmail.com");
-        mySendKeys(GWD.getDriver().findElement(By.cssSelector("input[id='password']")),"samO.@04#");
-        mySendKeys(GWD.getDriver().findElement(By.cssSelector("input[id='password-confirmation']")),"samO.@04#");
+    public void userFillsInTheBlanks(DataTable a) {
+        List<List<String>> list = a.asLists(String.class);
+        for (List<String> e : list) {
+            WebElement s = nu.getWebElement(e.get(0));
+            mySendKeys(s, e.get(1));
+        }
+//        mySendKeys(GWD.getDriver().findElement(By.cssSelector("input[id='firstname']")), "samo");
+//        mySendKeys(GWD.getDriver().findElement(By.cssSelector("input[id='lastname']")), "sam");
+//        mySendKeys(GWD.getDriver().findElement(By.cssSelector("input[id='email_address']")), "samAos@hotmail.com");
+//        mySendKeys(GWD.getDriver().findElement(By.cssSelector("input[id='password']")), "samO.@04#");
+//        mySendKeys(GWD.getDriver().findElement(By.cssSelector("input[id='password-confirmation']")), "samO.@04#");
 
     }
 
     @And("User clicks submit button")
-    public void userClicksSubmitButton() {
-        myClick(GWD.getDriver().findElement(By.cssSelector("button[type='submit'][title='Create an Account']")));
+    public void userClicksSubmitButton(DataTable a) {
+        List<String> s = a.asList(String.class);
+        for (String x : s) {
+            WebElement d = nu.getWebElement(x);
+            myClick(d);
+        }
+        //   myClick(GWD.getDriver().findElement(By.cssSelector("button[type='submit'][title='Create an Account']")));
     }
 
     @Then("User checks helper text")
-    public void userChecksHelperText() {
-
-        verifyContainsText(GWD.getDriver().findElement(By.cssSelector("[class='messages'] div")),"Thank you for registering with Main Website Store.");
+    public void userChecksHelperText(DataTable a) {
+        List<List<String>> s = a.asLists(String.class);
+        for (List<String> x : s) {
+            WebElement d = nu.getWebElement(x.get(0));
+            verifyContainsText(d, x.get(1));
+        }
     }
 }
 
