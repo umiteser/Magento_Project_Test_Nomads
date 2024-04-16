611da3724d7b3197ddd5582b8464b2e84f07f8da
Ayse Ihtiyar <ayseeihtiyar@gmail.com>
2024-04-16 16:59:54 +0300

    Tab_Menu_Pom WebElement added.

--- src/test/java/Step_Definitions/TAB_Menu/Tab_Menu_Pom.java
+++ src/test/java/Step_Definitions/TAB_Menu/Tab_Menu_Pom.java
@@ -1,11 +1,50 @@
 package Step_Definitions.TAB_Menu;
 
+import Pages.ParentPage;
 import Utilities.GWD;
-import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.*;
 
-public class Tab_Menu_Pom {
+import java.util.List;
+
+public class Tab_Menu_Pom extends ParentPage {
 
     public Tab_Menu_Pom() {
         PageFactory.initElements(GWD.getDriver(), this);
     }
+
+    @FindBy(xpath = "//div[@class='panel header']/ul/li[2]/a")
+    public WebElement singIn;
+
+    public WebElement getWebElementPageHeader(String strElement) {
+        switch (strElement) {
+            case "singIn":
+                return this.singIn;
+        }
+        return null;
+    }
+
+    @FindBy(id = "email")
+    public WebElement email;
+
+    @FindBy(name = "login[password]")
+    public WebElement password;
+
+    @FindBy(css = "button[class='action login primary']")
+    public WebElement singInButton;
+
+    @FindBy(css = "[id='ui-id-2'] li")
+    public List<WebElement> tabMenu;
+
+    @FindBy(css = "[id='ui-id-2']>li")
+    public List<WebElement> mainCategory;
+
+    @FindBy(css = "[class='item']>a")
+    public List<WebElement> sidebar;
+
+    @FindBy(xpath = "//div[@id='narrow-by-list']/div[1]//div[1]")
+    public WebElement sidebarCategory;
+
+    @FindBy(xpath = "//div[@id='narrow-by-list']/div[1]//div[2]/ol/li")
+    public List<WebElement> sidebarCategoryList;
 }
